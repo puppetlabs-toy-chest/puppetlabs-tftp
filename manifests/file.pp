0261c1d575caaa17ab7cfa162491fc0a7d82bd39
Thomas Van Doren <thomas.vandoren@gmail.com>
2012-06-15 15:54:32 -0700

    Fix spacing to meet puppet style guide and update new param defaults.
    The purge, replace, and recurselimit defaults are now undef.

--- manifests/file.pp
+++ manifests/file.pp
@@ -9,30 +9,30 @@
 # Usage:
 #
 define tftp::file (
-  $ensure  = file,
-  $recurse = false,
-  $purge   = false,
-  $replace = true,
-  $recurselimit = false,
-  $owner   = 'tftp',
-  $group   = 'tftp',
-  $mode    = '0644',
-  $content = undef,
-  $source  = undef
+  $ensure       = file,
+  $recurse      = false,
+  $purge        = undef,
+  $replace      = undef,
+  $recurselimit = undef,
+  $owner        = 'tftp',
+  $group        = 'tftp',
+  $mode         = '0644',
+  $content      = undef,
+  $source       = undef
 ) {
   include 'tftp'
 
   file { "${tftp::directory}/${name}":
-    ensure  => $ensure,
-    recurse => $recurse,
-    purge   => $purge,
-    replace => $replace,
+    ensure       => $ensure,
+    recurse      => $recurse,
+    purge        => $purge,
+    replace      => $replace,
     recurselimit => $recurselimit,
-    owner   => $owner,
-    group   => $group,
-    mode    => $mode,
-    content => $content,
-    source  => $source,
-    require => Class['tftp'],
+    owner        => $owner,
+    group        => $group,
+    mode         => $mode,
+    content      => $content,
+    source       => $source,
+    require      => Class['tftp'],
   }
 }
